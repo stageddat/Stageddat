d06d39ba96a1343b9508a3bbf95d3edb1305f97a
Stageddat <hello@stageddat.dev>
2026-06-03 00:32:55 +0200

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
+  site: "https://stageddat.dev",
 });
